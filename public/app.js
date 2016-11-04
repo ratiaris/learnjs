5a629d723a4af6bef17aeb30587501be762c0b01
Benjamin Ratiarisolo <benjamin.ratiarisolo@gmail.com>
2016-11-04 13:52:12 +0100

    pass jasmine test

---
+++ public/app.js
@@ -0,0 +1,7 @@
+'use strict'
+let learnJS = {
+	showView(hash) {
+		let problemView = $('<div class="problem-view">').text('Coming soon!');
+		$('.view-container').empty().append(problemView);
+	}
+};
