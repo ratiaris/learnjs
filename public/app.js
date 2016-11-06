3d3938592fc3042ed013b8deddd66c09c35fee66
Benjamin Ratiarisolo <benjamin.ratiarisolo@gmail.com>
2016-11-06 15:11:41 +0100

    Chapter 2, Loading the application, responding to events

--- public/app.js
+++ public/app.js
@@ -1,25 +1,57 @@
 'use strict'
 let learnJS = {
 
-	problemView() {
+	_ROUTES : new Map(),
+
+	problemView(id) {
+		// console.log(`problemView ${id}`);
 		let newView = document.createElement('div');
-		newView.textContent = 'Coming soon!';
+		newView.textContent = `Problem #${id} coming soon!`;
 		newView.className = 'problem-view';
+		// console.log(newView);
 		return newView;
 	},
 
-	showView(hash) {
-		let routes = {
-			'#problem-1': this.problemView
-		};
-		let viewFunction = routes[hash];
-		if (viewFunction) {
-			let viewContainer = document.querySelector('.view-container');
-			while(viewContainer.firstChild) {
-		 	   viewContainer.removeChild( viewContainer.firstChild);
-			}
-			viewContainer.appendChild(viewFunction());
-			console.log(viewContainer);
+	_initializeRoutes(routes) {
+		routes.set('#problem', this.problemView);
+	},
+
+	_getRoute(name) {
+		let routes = this._ROUTES;
+		if (routes.size === 0) {
+			this._initializeRoutes(routes);
 		}
+		return routes.get(name);
+	},
+
+	showView(hash) {
+		// console.log(`showView ${hash}`);
+		// console.log(this);
+		let hashParts = hash.split('-');
+		let viewName = hashParts[0];
+		let viewParameter = hashParts[1];
+		// console.log(viewName);
+		// console.log(viewParameter);
+		let viewFunction = this._getRoute(viewName);
+		// let viewFunction = learnJS.problemView;
+		if (viewFunction && viewParameter) {
+			let viewContainer = document.querySelector('.view-container');
+			// console.log(viewContainer);
+			if (viewContainer !== null) {
+				while(viewContainer.firstChild) {
+			 	   viewContainer.removeChild( viewContainer.firstChild);
+				}
+				// viewFunction(viewParameter) does not work for Jasmine tests that
+				// spy on learnJS.problemView(id) when invoking learnJS.showView(hash)
+				let child = this[viewFunction.name](viewParameter);
+				viewContainer.appendChild(child);
+			}
+		}
+	},
+
+	domContentLoaded() {
+		learnJS.showView(window.location.hash);
 	}
 }; 
+
+window.addEventListener('DOMContentLoaded', learnJS.domContentLoaded);
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -5,8 +5,28 @@ describe('Serverless Single Page App', () => {
 		expect($('.view-container .problem-view').length).toEqual(1);
 	});
 
-	it ('shows the landing page when there is no hash', () => {
+	it('shows the landing page when there is no hash', () => {
 		learnJS.showView('');
 		expect($('.view-container .landing-view').length).toEqual(1);
+	});
+
+	it('passes the hash view parameter to the view function', () => {
+		spyOn(learnJS, 'problemView').and.callThrough();
+		learnJS.showView('#problem-42');
+		expect(learnJS.problemView).toHaveBeenCalledWith('42');
+	});
+
+	it ('invokes the router when loaded', () => {
+		spyOn(learnJS, 'showView');
+		learnJS.domContentLoaded();
+		expect(learnJS.showView).toHaveBeenCalledWith(window.location.hash);
+	});
+
+	describe('problem view', () => {
+		it('has some text content that includes the problem number', () => {
+			let id = '33';
+			let view = learnJS.problemView(id);
+			expect(view.textContent).toEqual(`Problem #${id} coming soon!`);
+		});
 	});
 });
